e97999b7f0111ff3e80f5bcb39881464096eb090
seymourjunk <gololobova.galya@gmail.com>
2023-04-06 18:01:17 +0300

    Add project files.

---
+++ Macros.cpp
@@ -0,0 +1,18 @@
+// Macros.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    printf(
+        "Press C to create space"
+    );
+
+    //printf(
+    //    "-c     create space"
+    //    "-r     delete space"
+    //    "-n     rename space"
+    //    "-l     list of spaces"
+    //);
+}
